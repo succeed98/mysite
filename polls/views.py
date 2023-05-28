f63a4531efb2b359c94ae800ca6221de5e9e4eb5
Fourteen98 <emuhiydeen6@gmail.com>
2023-05-28 15:56:12 +0000

    before improving views

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
 from django.db.models import F
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 # Create your views here.
@@ -14,7 +15,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions"""
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 
 class DetailView(generic.DetailView):
